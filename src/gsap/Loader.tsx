cf2010c8b7cd830726b924be1d2267f101c98965
Dale B <daleban.dev@gmail.com>
2023-10-17 22:46:27 +0800

    feat: setup gsap loader

---
+++ src/gsap/Loader.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { gsap } from "gsap";
+import useLayoutEffect from "@/hooks/useIsomorphicLayoutEffect";
+import { useGlobal } from "@/hooks/useGlobalContext";
+
+export const Loader = () => {
+  const { unmountLoader, handleUnmountLoader } = useGlobal();
+
+  useLayoutEffect(() => {
+    const tl: GSAPTimeline = gsap.timeline({
+      delay: 1,
+      ease: "power3.inOut",
+    });
+
+    if (!unmountLoader) {
+      tl.to("#loader-bg", {
+        duration: 1.5,
+        ease: "power3.inOut",
+        opacity: 0,
+        onComplete: () => handleUnmountLoader(),
+      });
+    }
+  }, [handleUnmountLoader, unmountLoader]);
+
+  return (
+    !unmountLoader && (
+      <div
+        id="loader-bg"
+        className="fixed left-0 top-0 z-50 h-screen w-screen bg-black"
+      />
+    )
+  );
+};
