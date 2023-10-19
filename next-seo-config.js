aa56bf42e51a09a8980f0f37592c6a924dc5af21
Dale B <daleban.dev@gmail.com>
2023-10-20 04:55:14 +0800

    feat: setup next seo

---
+++ next-seo-config.js
@@ -0,0 +1,33 @@
+const description =
+  "Embrace the Smiles, Feel the Rhythm: Join the Festivities, and Celebrate with Us!";
+
+export default {
+  title: "Masskara Festival 2023",
+  description,
+  openGraph: {
+    type: "website",
+    url: "https://masskara-2023.vercel.app",
+    title: "Masskara Festival 2023",
+    description,
+    images: [
+      {
+        url: "/images/banner.jpg",
+        wjpgidth: 1400,
+        height: 800,
+        alt: "omsimos",
+        type: "image/png",
+      },
+    ],
+    site_name: "Masskara Festival 2023",
+  },
+  twitter: {
+    cardType: "summary_large_image",
+  },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+      type: "image/x-icon",
+    },
+  ],
+};
