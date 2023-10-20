4e42b7ebf83397943be717ac4e4aaae2c470f74f
Dale B <daleban.dev@gmail.com>
2023-10-20 09:54:57 +0800

    chore: add next seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import { GlobalContextProvider } from "@/hooks/useGlobalContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import SEO from "../../next-seo-config";
+import { DefaultSeo } from "next-seo";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalContextProvider>
       <Layout>
+        <DefaultSeo {...SEO} />
         <Component {...pageProps} />;
       </Layout>
     </GlobalContextProvider>
